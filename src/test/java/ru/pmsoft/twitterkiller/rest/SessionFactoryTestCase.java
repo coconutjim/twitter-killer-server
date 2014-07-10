d03ccc7e3fcebe6a3e95c950f479ab87eb10b651
LevOsipovHSE <osipov.enterprise@gmail.com>
2014-07-10 17:46:11 +0400

    tests for SessionFactory

---
+++ src/test/java/ru/pmsoft/twitterkiller/rest/SessionFactoryTestCase.java
@@ -0,0 +1,67 @@
+package ru.pmsoft.twitterkiller.rest;
+
+
+import org.testng.annotations.Test;
+import ru.pmsoft.twitterkiller.domain.entity.User;
+import ru.pmsoft.twitterkiller.domain.entity.UserSession;
+import ru.pmsoft.twitterkiller.domain.factory.SessionFactory;
+import ru.pmsoft.twitterkiller.domain.util.StringGenerator;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.testng.Assert.assertEquals;
+
+import static org.mockito.Mockito.mock;
+
+public class SessionFactoryTestCase {
+
+    private static SessionFactory createSystemUnderTest(StringGenerator tokenGenerator) {
+        return new SessionFactory(tokenGenerator == null ? mock(StringGenerator.class) : tokenGenerator);
+    }
+
+    private User dummyUser = new User("LeoTheMagnificent");
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void constructor_ifTokenIsNull_shouldThrowIllegalArgumentException() {
+        new SessionFactory(null);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void setTokenLifeTime_ifLifeTimeBelowZero_shouldThrowIllegalArgumentException() {
+        SessionFactory sut = createSystemUnderTest(null);
+        sut.setTokenLifeTime(-1);
+    }
+
+    @Test
+    public void expirationComputingTest() {
+        int testTokenLifetime = 100000;
+        Calendar testExpirationTime = new GregorianCalendar();
+        testExpirationTime.setTime(new Date());
+        testExpirationTime.add(Calendar.SECOND, testTokenLifetime);
+
+        SessionFactory sut = createSystemUnderTest(null);
+        sut.setTokenLifeTime(testTokenLifetime);
+        UserSession testSession = sut.create(dummyUser);
+
+        assertEquals(Math.abs(testSession.getExpiration().getTime() - testExpirationTime.getTime().getTime()) < 1000,
+                true);
+    }
+
+    @Test
+    public void tokenGeneratorTest() {
+        final String testString = "LeoTester";
+        StringGenerator testStringGenerator = new StringGenerator() {
+            @Override
+            public String generate() {
+                return testString;
+            }
+        };
+
+        SessionFactory sut = createSystemUnderTest(testStringGenerator);
+        UserSession testSession = sut.create(dummyUser);
+
+        assertEquals(testSession.getToken(), testString);
+    }
+}
